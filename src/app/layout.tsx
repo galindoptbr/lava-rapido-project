2c5d9ed7d76fef4195fb5e71df950e0f89eaefc4
Galindo Leite <galindoleitept@gmail.com>
2024-05-29 22:34:30 +0100

    test manifest nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Lava Rapido Galp",
   description: "Fechamento de folha para Lava Rapidos",
+  manifest: "/webmanifest.json",
 };
 
 export default function RootLayout({
@@ -16,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt">
-      <body className="bg-[#E7E6E4] text-[#403C3D]">
-        {children}
-      </body>
+      <body className="bg-[#E7E6E4] text-[#403C3D]">{children}</body>
     </html>
   );
 }
