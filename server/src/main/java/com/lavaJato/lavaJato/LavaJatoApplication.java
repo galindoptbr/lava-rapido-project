9b3dc2825663eae8bac09d7674e8670664a13659
Andzuo <alucasm02@gmail.com>
2024-06-14 02:30:19 -0300

    TODO: Criar endpoint para relatorio em xlsx e tabela e lista por calendário

---
+++ server/src/main/java/com/lavaJato/lavaJato/LavaJatoApplication.java
@@ -0,0 +1,13 @@
+package com.lavaJato.lavaJato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavaJatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavaJatoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/lavaJato/lavaJato/LavaJatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavaJato.lavaJato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavaJatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
